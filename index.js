ac72b30be8a5e9b50ead2755b3cebcf36c06ca7a
smile995 <ah.smile995@gmail.com>
2025-01-16 11:45:55 +0600

    Refactor some logic

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const app = express()
-const cors = require('cors');
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = 5000;
-require('dotenv').config()
+require("dotenv").config();
 // middleweres
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6uwuu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,7 +15,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,40 +28,44 @@ async function run() {
     const cartCollection = database.collection("carts");
     const reviewCollection = database.collection("reviews");
     // foodCollection related CRUD operations
-    app.get("/foods",async(req,res)=>{
-        const result= await foodCollection.find().toArray();
-        res.status(200).send(result)
-    })
+    app.get("/foods", async (req, res) => {
+      const result = await foodCollection.find().toArray();
+      res.status(200).send(result);
+    });
 
-    app.post("/foods",async(req,res)=>{
-      const food= req?.body;
-      const result= await foodCollection.insertOne(food);
-      res.send(result).status(200)
-    })
+    app.post("/foods", async (req, res) => {
+      const food = req?.body;
+      const result = await foodCollection.insertOne(food);
+      res.send(result).status(200);
+    });
     // cartCollection related CRUD operations
-    app.get('/carts',async(req,res)=>{
-      const result= await cartCollection.find().toArray();
-      res.status(200).send(result)
-    })
+    app.get("/carts", async (req, res) => {
+      const email = req?.query?.email;
+      const query = { userEmail: email };
+      const result = await cartCollection.find(query).toArray();
+      res.status(200).send(result);
+    });
 
-    app.post("/carts",async(req,res)=>{
-      const cart= req?.body;
-      const result= await cartCollection.insertOne(cart);
-      res.send(result).status(200)
-    })
+    app.post("/carts", async (req, res) => {
+      const cart = req?.body;
+      const result = await cartCollection.insertOne(cart);
+      res.send(result).status(200);
+    });
     // reviewCollection related CRUD operations
-    app.get('/reviews',async(req,res)=>{
-      const result= await reviewCollection.find().toArray();
-      res.status(200).send(result)
-    })
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.status(200).send(result);
+    });
 
-    app.post("/reviews",async(req,res)=>{
-      const review= req?.body;
-      const result= await reviewCollection.insertOne(review);
-      res.send(result).status(200)
-    })
+    app.post("/reviews", async (req, res) => {
+      const review = req?.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result).status(200);
+    });
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -70,10 +73,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
